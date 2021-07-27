f608af517908af7202a06b78a70b952d1bb59720
Seongsangcho <liasqui31@gmail.com>
2021-07-27 12:03:33 +0900

    refactor: Intersect Observer hooks 분리

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import "./App.css";
 import { getCommentData } from "./api/getCommentData";
+import useIntersectObserver from "./hooks/useIntersectObserver";
 /*
 ! data fetching done
-* 스크롤 맨 밑에 다다랐을 때, 다음 페이지 넘버를 갖게되는 hook
-* 피그마 디자인 퍼블리싱
+! 스크롤 맨 밑에 다다랐을 때, 다음 페이지 넘버를 갖게되는 hook
+! 피그마 디자인 퍼블리싱
 */
 
 function App() {
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
   const [commentList, setCommentList] = useState([]);
   const intersectRef = useRef(null);
-  useEffect(async () => {
-    const data = await getCommentData(page);
-    setCommentList([...commentList, ...data]);
-    console.log(page);
-  }, [page]);
+  const { isIntersect } = useIntersectObserver(intersectRef);
 
-  const handleObserver = useCallback(async (entries) => {
-    const target = entries[0];
-    if (target.isIntersecting) {
-      console.log("is intersect");
+  useEffect(async () => {
+    if (isIntersect) {
+      const data = await getCommentData(page);
+      setCommentList([...commentList, ...data]);
       setPage((prev) => prev + 1);
     }
-  }, []);
-  const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.01,
-  };
-  useEffect(() => {
-    const observer = new IntersectionObserver(handleObserver, options);
-    if (intersectRef.current) observer.observe(intersectRef.current);
-    return () => observer.disconnect();
-  }, [handleObserver]);
+  }, [isIntersect]);
   return (
     <>
       <div className="container">
@@ -60,7 +47,7 @@ function App() {
           })}
         </ul>
         <div id="intersectElement" ref={intersectRef}>
-          intersect
+          Loading...
         </div>
       </div>
     </>
---
+++ src/hooks/useIntersectObserver.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect, useCallback } from "react";
+
+const useIntersectObserver = (intersectRef) => {
+  const [isIntersect, setIsIntersect] = useState(false);
+
+  const handleObserver = useCallback(async (entries) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      setIsIntersect(true);
+    } else {
+      setIsIntersect(false);
+    }
+  }, []);
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.01,
+  };
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleObserver, options);
+    if (intersectRef.current) observer.observe(intersectRef.current);
+    return () => observer.disconnect();
+  }, [handleObserver]);
+  return {
+    isIntersect,
+  };
+};
+
+export default useIntersectObserver;

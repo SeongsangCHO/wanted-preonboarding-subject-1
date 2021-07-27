79932802dbe918d26c7f0c8be73a85a671916240
Seongsangcho <liasqui31@gmail.com>
2021-07-27 10:19:50 +0900

    feat: 코멘트 데이터 불러오는 함수 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
-
+/*
+* data fetching
+* 스크롤 맨 밑에 다다랐을 때, 다음 페이지 넘버를 갖게되는 hook
+* 피그마 디자인 퍼블리싱
+*/
 function App() {
   return (
     <></>
---
+++ src/api/getCommentData.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API_ENDPOINT = `https://jsonplaceholder.typicode.com/comments`;
+const LIMIT = 10;
+/*
+ * _page = 1.. ++
+ * _limit = 10 fix
+ */
+
+const getCommentData = async (page) => {
+  try {
+    const res = await axios.get(`${API_ENDPOINT}`, {
+      params: {
+        _page: page,
+        _limit: LIMIT,
+      },
+    });
+    console.log(res.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
